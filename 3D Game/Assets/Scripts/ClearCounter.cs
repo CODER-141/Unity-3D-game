b6f25d9dfca391bcd1700f78328a781058571d7c
Thimira00 <thimiraushan2000@gmail.com>
2024-06-27 23:44:18 +0530

    updated the kitchen object parent

--- 3D Game/Assets/Scripts/ClearCounter.cs
+++ 3D Game/Assets/Scripts/ClearCounter.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour
-{
+public class ClearCounter : MonoBehaviour{
+
+
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondCounterCounter;
+    [SerializeField] private bool testing;
 
+
+    private KitchenObject kitchenObject;
+
+
+    private void Update() {
+        if(testing && Input.GetKeyDown(KeyCode.T)) {
+            if(kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondCounterCounter);
+            }
+        }
+    }
     public void Interact() {
-        Debug.Log("Interact");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.localPosition = Vector3.zero;
+
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
     }
 }
