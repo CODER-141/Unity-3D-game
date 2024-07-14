c96b6072b29e46d2790d2c5b3d2b2f8473068e2c
Thimira00 <thimiraushan2000@gmail.com>
2024-07-14 16:42:26 +0530

    updated the plate complete visual

--- 3D Game/Assets/Scripts/PlateKitchenObject.cs
+++ 3D Game/Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlateKitchenObject : KitchenObject{
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
 
     [SerializeField] private List<KitchenObjectSO> validkitchenObjectSOList;
 
@@ -21,6 +29,10 @@ public class PlateKitchenObject : KitchenObject{
             return false;
         }else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectSO = kitchenObjectSO
+            });
             return true;
         }
     }
